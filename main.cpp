427e11b8a0295190d193ac4182f6843cd761715a
5612466 <115354030+5612466@users.noreply.github.com>
2022-12-08 17:47:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+# include "lib.h"
+using namespace std;
 
 int main(){
+   char a;
+    cin>>a;
+    if (verifica(a)==true){
+        cout<<lettera(a)<<endl;
+    }
+    else{
+        cout<<"errore"<<endl;
+    }
   return 0;
 }
